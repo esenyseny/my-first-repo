512bd121ff312407c0b76062c9b968b704f0008e
esenyseny <einherzaufallen@gmail.com>
2025-04-14 23:56:23 +0300

    local commit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
 #include <iostream>
-int main() { std::cout << "v10 amended version" << std::endl; return 0; }
+int main() { std::cout << "hello, I'am from local repo" << std::endl; return 0; }
 // версия 2
 // версия 3
